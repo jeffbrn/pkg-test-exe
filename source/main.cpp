4b4440ae7160eb533ec01dc5dfb9bd169c3e2e15
jeffbrn <jeff.brn@gmail.com>
2023-02-12 16:08:05 -0700

    link to lib

--- source/main.cpp
+++ source/main.cpp
@@ -2,11 +2,16 @@
 #include <string>
 
 #include "lib.hpp"
+#include "test-lib/test-lib.hpp"
 
 auto main() -> int
 {
   auto const lib = library {};
   auto const message = "Hello from " + lib.name + "!";
   std::cout << message << '\n';
+
+  auto const tlib = new exported_class();
+  std::cout << "external lib: " << tlib->name() << std::endl;
+  delete tlib;
   return 0;
 }
